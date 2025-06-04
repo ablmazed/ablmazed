3cfd214bde00dcbadeeb19d4d9c3b09b6f3be826
mazed <ablmazed@gmail.com>
2025-06-04 10:46:28 +0600

    update 15

--- components/main/skills.tsx
+++ components/main/skills.tsx
@@ -1,19 +1,13 @@
-import { SkillDataProvider } from "@/components/sub/skill-data-provider";
-import { SkillText } from "@/components/sub/skill-text";
+import { SkillDataProvider } from '@/components/sub/skill-data-provider'
+import { SkillText } from '@/components/sub/skill-text'
 
-import {
-  BACKEND_SKILL,
-  FRONTEND_SKILL,
-  FULLSTACK_SKILL,
-  OTHER_SKILL,
-  SKILL_DATA,
-} from "@/constants";
+import { BACKEND_SKILL, FRONTEND_SKILL, SKILL_DATA } from '@/constants'
 
 export const Skills = () => {
   return (
     <section
       id="skills"
-      style={{ transform: "scale(0.9)" }}
+      style={{ transform: 'scale(0.9)' }}
       className="flex flex-col items-center justify-center gap-3 h-full relative overflow-hidden py-20"
     >
       <SkillText />
@@ -55,30 +49,6 @@ export const Skills = () => {
           />
         ))}
       </div>
-      <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
-        {FULLSTACK_SKILL.map((skill, i) => (
-          <SkillDataProvider
-            key={skill.skill_name}
-            src={skill.image}
-            name={skill.skill_name}
-            width={skill.width}
-            height={skill.height}
-            index={i}
-          />
-        ))}
-      </div>
-      <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
-        {OTHER_SKILL.map((skill, i) => (
-          <SkillDataProvider
-            key={skill.skill_name}
-            src={skill.image}
-            name={skill.skill_name}
-            width={skill.width}
-            height={skill.height}
-            index={i}
-          />
-        ))}
-      </div>
 
       <div className="w-full h-full absolute">
         <div className="w-full h-full z-[-10] opacity-30 absolute flex items-center justify-center bg-cover">
@@ -95,5 +65,5 @@ export const Skills = () => {
         </div>
       </div>
     </section>
-  );
-};
+  )
+}

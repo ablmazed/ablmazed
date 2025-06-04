5ca57393d075a63565cdcde8354a2bd82d74d7e9
mazed <ablmazed@gmail.com>
2025-06-04 10:13:05 +0600

    update 10

--- constants/index.ts
+++ constants/index.ts
@@ -259,25 +259,25 @@ export const OTHER_SKILL = [
 
 export const PROJECTS = [
   {
-    title: 'Modern Next.js 14 Portfolio',
+    title: 'Modern Next.js 14 Ecommerce',
     description:
-      'Embark on a journey through my professional evolution with the "Modern Next.js Portfolio" - a dynamic showcase of my skills, experiences, and passion for web development. Crafted with precision and powered by Next.js, this portfolio is more than just a static display; it\'s an immersive experience that reflects the cutting edge of modern web technologies.',
-    image: '/projects/project-1.png',
-    link: 'https://example.com',
+      'Embark on a journey through my professional evolution with the "Modern Next.js Ecommerce" - a dynamic showcase of my skills, experiences, and passion for web development. Crafted with precision and powered by Next.js, this portfolio is more than just a static display; it\'s an immersive experience that reflects the cutting edge of modern web technologies.',
+    image: '/projects/fashionhouse.png',
+    link: 'https://nextjs-fashion-house.vercel.app',
   },
   {
-    title: 'Interactive Cards Portfolio',
+    title: 'XiroHomes- Real State Project',
     description:
-      'Step into the extraordinary world of my professional journey through the "Interactive Cards Portfolio" - an innovative and visually captivating platform that redefines the traditional portfolio experience. Ditching the conventional static layout, this portfolio leverages interactive cards to showcase my skills, projects, and personality in an engaging and dynamic manner.',
-    image: '/projects/project-2.png',
-    link: 'https://example.com',
+      'Step into the extraordinary world of my professional journey through the "XiroHomes- Real State Project" - an innovative and visually captivating platform that redefines the traditional portfolio experience. Ditching the conventional static layout, this portfolio leverages interactive cards to showcase my skills, projects, and personality in an engaging and dynamic manner.',
+    image: '/projects/xirohomes.png',
+    link: 'https://xiro-homes.vercel.app',
   },
   {
-    title: 'Space Themed Website',
+    title: 'Amazon Clone Website- Nextjs Amazona',
     description:
       'Embark on an interstellar journey with my "Space Themed Website", a mesmerizing space-themed website that invites you to explore the cosmic wonders beyond our world. Immerse yourself in an awe-inspiring digital experience that blends cutting-edge design with the mysteries of the universe.',
-    image: '/projects/project-3.png',
-    link: 'https://example.com',
+    image: '/projects/amazon.png',
+    link: 'https://nextjs-amazona-ecommerce.vercel.app',
   },
 ] as const
 
